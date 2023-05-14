317b4035011333ff00a138332c05bf51e4220666
Josh Onadele <joshuaonadele@gmail.com>
2023-05-14 08:25:00 -0700

    Just doing my damn assignment

---
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# get the absolute path name of the current working directory
+cwd=$(pwd)
+
+# print the absolute path name of the current working directory
+echo "The absolute path name of the current working directory is: $cwd"
+
